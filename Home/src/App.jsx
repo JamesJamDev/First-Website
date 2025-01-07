1aed7bc4ce213c4357c09b50b353db56780ccd28
JamesJohnson2005 <jamejohn2005@gmail.com>
2025-01-07 12:50:46 -0500

    General Boxception Info has been added

--- Home/src/App.jsx
+++ Home/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import jellyJamLogo from "./assets/jellyjam.png";
+import steamicon from "./assets/steamlogo.png"
 import './App.css'
 import axios from "axios";
 
@@ -29,10 +30,20 @@ function App() {
             <img src={jellyJamLogo} className="jellyjam" alt="JellyJam Logo" style={{ width: '100px', height: '100px' }} />
           </a>
         </div>
-        <div>
-          <button onClick={() => setCount(count + 1)}>Button</button>
-        </div>
-        <textarea value={array} readOnly></textarea>
+        <h1 style={{fontSize: '50px'}}>Projects</h1>
+      </div>
+      <div margin="auto" style={{textAlign: 'center', whiteSpace: 'pre-wrap'}}>
+        <h1>Boxception</h1>
+        <text style={{fontSize: '20px'}}>A 3D puzzle game where you use boxes to solve puzzles. &#10; While also
+          taking control of items in the environment to give you a whole new set of abilities.</text>
+          <div>
+            <h2>Boxception Steam Page</h2>
+          </div>
+          <div>
+            <a href="https://store.steampowered.com/app/1729280/Boxception/" target="_blank"></a>
+            <img src={steamicon} alt="Steam Logo" style={{width: '50px', height: '50px'}} />
+          </div>
+          
       </div>
     </>
   )
